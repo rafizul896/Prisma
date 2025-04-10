c7392f950fa84f7e6053ae9eed629803f768625f
rafizul896 <rafizulislam896@gmail.com>
2025-04-10 19:21:02 +0600

    Inserting Data into the Database with Relationships

--- src/create.ts
+++ src/create.ts
@@ -3,52 +3,49 @@ import { PrismaClient } from "../generated/prisma";
 const prisma = new PrismaClient();
 
 const main = async () => {
+  // const createUser = await prisma.user.create({
+  //     data: {
+  //         userName: "user",
+  //         email: "user@gmail.com",
+  //         role: "user",
+  //       }
+  // })
+  // console.log(createUser);
+  // const createProfile = await prisma.profile.create({
+  //     data: {
+  //         bio: 'This is user Bio',
+  //         userId: 10
+  //       }
+  // })
+  // console.log(createProfile)
+  //   const createCategory = await prisma.category.create({
+  //     data: {
+  //         name: 'Programming',
+  //     }
+  //   })
+  //   console.log(createCategory)
 
-    // create data into db
-    // const result = await prisma.post.create({
-    //     data: {
-    //         title: "This is title",
-    //         content: "This is content",
-    //         authorName: "Rafizul Islam"
-    //     }
-    // })
+  const createPost = await prisma.post.create({
+    data: {
+      title: "This is user Post - 2",
+      content: "This is user Post content............",
+      authorId: 10,
+      postCategory: {
+       create: {
+        category: {
+            connect: {
+                id: 3
+            }
+        }
+       }
+      },
+    },
+    include: {
+        postCategory: true
+    }
+  });
 
-    // create many
-    const createMany = await prisma.post.createMany({
-        data: [
-            {
-              title: "Learning Prisma ORM",
-              content: "Prisma makes working with databases much easier.",
-              published: true,
-              authorName: "Rafizul Islam Rafiz",
-            },
-            {
-              title: "React with Tailwind",
-              content: "Using Tailwind CSS in React helps you build fast and beautiful UIs.",
-              published: false,
-              authorName: "Tanvir Hasan",
-            },
-            {
-              title: "Node.js for Backend",
-              content: "Node.js allows JavaScript to run on the server-side.",
-              published: true,
-              authorName: "Shamim Reza",
-            },
-            {
-              title: "MongoDB Basics",
-              content: "MongoDB is a NoSQL database that stores data in JSON-like documents.",
-              published: false,
-              authorName: "Fahim Rahman",
-            },
-            {
-              title: "Express.js Crash Course",
-              content: "Express.js is a fast, minimal web framework for Node.js.",
-              published: true,
-            },
-          ],
-    });
-
-    console.log(createMany);
+  console.log(createPost);
 };
 
-main()
+main();
